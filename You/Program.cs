359a5dbf8dad08f3b0dc67e6e50e2a1127ecb84d
moimoiblanc <moimoiblanc@gmail.com>
2015-12-08 23:51:19 +0900

    メインプロジェクトを追加

---
+++ You/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace You
+{
+    /// <summary>
+    /// プログラムのエントリポイントを提供します。
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// プログラムを開始します。
+        /// </summary>
+        public static void Main()
+        {
+        }
+    }
+}
